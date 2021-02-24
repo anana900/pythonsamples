bdad0749eb0bf41ee18e543b39a62bc7b837ddcc
anana <anana900@wp.pl>
2021-02-24 23:21:28 +0100

    socket, select:naive chatroom

---
+++ src/sockets/sclient.py
@@ -0,0 +1,39 @@
+'''
+Created on Feb 21, 2021
+
+@author: ananacd
+'''
+import socket as soc
+import scommon as hs
+import sys
+import select
+
+class Client():
+    def __init__(self, server_port=65432, server_host=soc.gethostbyname(soc.gethostname())):
+        self.address = (server_host, server_port)
+        
+        self.client = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
+        self.client.connect(self.address)
+        
+        print("Client is connecting to {0[0]}:{0[1]}".format(self.address))
+
+    def run(self, data):
+        while True:
+            read_socket_list = [sys.stdin, self.client]
+            read_s, write_s, ex_s = select.select(read_socket_list, [], [])
+
+            for rs in read_s:
+                if rs == self.client:
+                    data_from_server = hs.socket_message_recv(self.client)
+                    print("{}".format(data_from_server))
+                else:
+                    data = input(">: ")
+                    hs.socket_message_send(self.client, data)
+
+                    if data == "koniec":
+                        self.client.close()
+                        break
+
+if __name__ == '__main__':
+    client = Client(int(sys.argv[1]))
+    client.run("Czesc, co slychac?")
---
+++ src/sockets/sserver.py
@@ -0,0 +1,78 @@
+'''
+Created on Feb 21, 2021
+
+@author: anana
+'''
+import socket as soc
+import threading as th
+import scommon as hs
+
+class Server():
+    
+    QUEUE_NO_CONN = 10
+    chatrooms = {"Boys":{}, "Girls":{}}
+    klienci = dict()
+
+    def __init__(self, port=65432, host=soc.gethostbyname(soc.gethostname())):
+        self.server_address = (host, port)
+        
+        self.server = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
+        self.server.bind(self.server_address)
+        
+        self.server.listen(self.QUEUE_NO_CONN)
+        
+        print("Server started: {0[0]}:{0[1]}".format(self.server_address))
+    
+    def chatroom_add_participant(self, client_socket, client_addr, message):
+        if "chatroom:" in message:
+            chatroom = message[9:]
+            if chatroom in self.chatrooms.keys():
+                chatroom_clients = self.chatrooms[chatroom]
+                chatroom_clients[client_addr] = client_socket
+                self.chatrooms.update({chatroom:chatroom_clients})
+                print("Client {} added to chatroom {}".format(client_addr, chatroom))
+                print("{} chatroom participants: {}".format(chatroom,self.chatrooms[chatroom].keys()))
+            else:
+                print("No such chatroom {}".format(chatroom))
+
+    def chatroom_broadcast(self, client_soc, client_addr, messg):
+        if "chatroom:" in messg:
+            return 0
+
+        for rooms, client_socks in self.chatrooms.items():
+            if client_addr in client_socks.keys():
+                for cls in client_socks.values():
+                    if cls != client_soc:
+                        hs.socket_message_send(cls, messg)
+
+    def handle_single_client(self, client_socket, client_address):
+        while True:
+            data_from_client = hs.socket_message_recv(client_socket)
+            print(data_from_client)
+            
+            self.chatroom_add_participant(client_socket, client_address, data_from_client)
+            self.chatroom_broadcast(client_socket, client_address, data_from_client)
+            
+            hs.socket_message_send(client_socket, "OK")
+        
+            if data_from_client == "koniec":
+                print("Rozlaczanie z klientem: {0[0]}:{0[1]}".format(client_address))
+                client_socket.close()
+                del self.klienci[th.currentThread().ident]
+                break        
+
+    def run(self):
+        while True:
+            client_soc, client_address = self.server.accept()
+            t = th.Thread(target=self.handle_single_client, args=(client_soc,client_address))
+            t.setDaemon(True)
+            t.start()
+            
+            self.klienci[t.ident] = client_address
+            print("Watki, połączenie z klientem: {0} {1[0]}:{1[1]}".format(t.ident, client_address))
+            
+        self.server.close()
+
+if __name__ == '__main__':
+    server = Server()
+    server.run()
