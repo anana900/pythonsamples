992df352040bbedb0364228dec0b7d866ed1d236
anana <anana900@wp.pl>
2022-12-08 09:37:09 +0100

    Zlaczenie metod w klasie w prostokatszukaj.

--- src/geometria/prostokatszukaj.py
+++ src/geometria/prostokatszukaj.py
@@ -21,35 +21,30 @@ class FindRectangle:
             remaining_rectangles.append((rect.x2 - rect.x1, rect.y2 - rect.y1, rect.x1, rect.y1))
         return remaining_rectangles
 
-    def _intersections(self, frame: Region, rect: Region) -> bool:
-        """Sprawdza czy rect jest wewnątrz frame - True. False jeśli wyszedł poza frame."""
-        return frame.x1 < rect.x2 and frame.x2 > rect.x1 and \
-               frame.y1 < rect.y2 and frame.y2 > rect.y1
-
-    def _clip_rectangle(self, frame: Region, rect: Region) -> Region:
-        """Wycina prostokąt większy od rect ale mniejszy od frame"""
-        return self.Region(max(frame.x1, rect.x1), max(frame.y1, rect.y1),
-                           min(frame.x2, rect.x2), min(frame.y2, rect.y2))
-
-    def _clip_rectangles(self, frame: Region, rects: list[Region]) -> list[Region]:
-        """Z listy istniejacych prostokątów wycina kolejne, w ramach framea"""
-        return [self._clip_rectangle(frame, r) for r in rects if self._intersections(frame, r)]
+    def _clip_rectangles(self, frame: Region, rectangles: list[Region]) -> list[Region]:
+        rectangle_list = []
+        for rectangle in rectangles:
+            if frame.x1 < rectangle.x2 and frame.x2 > rectangle.x1 and \
+                    frame.y1 < rectangle.y2 and frame.y2 > rectangle.y1:
+                rectangle_list.append(self.Region(max(frame.x1, rectangle.x1), max(frame.y1, rectangle.y1),
+                                                  min(frame.x2, rectangle.x2), min(frame.y2, rectangle.y2)))
+        return rectangle_list
 
     def _split_rectangles(self, frame: Region, rects: list[Region]) -> Region:
         if frame.x1 >= frame.x2 or frame.y1 >= frame.y2:
-            pass    # frame niepoprawny
+            pass
         elif not rects:
-            yield frame   # jeśli nie ma wewnątrz frame innych prostokątów to jedym wynikiem jest frame
+            yield frame
         else:
             selected = random.choice(rects)
-            above = self.Region(frame.x1, frame.y1, frame.x2, selected.y1)
-            left = self.Region(frame.x1, selected.y1, selected.x1, selected.y2)
-            right = self.Region(selected.x2, selected.y1, frame.x2, selected.y2)
-            below = self.Region(frame.x1, selected.y2, frame.x2, frame.y2)
-            yield from self._split_rectangles(above, self._clip_rectangles(above, rects))
-            yield from self._split_rectangles(left, self._clip_rectangles(left, rects))
-            yield from self._split_rectangles(right, self._clip_rectangles(right, rects))
-            yield from self._split_rectangles(below, self._clip_rectangles(below, rects))
+            rectangle_above = self.Region(frame.x1, frame.y1, frame.x2, selected.y1)
+            rectangle_left = self.Region(frame.x1, selected.y1, selected.x1, selected.y2)
+            rectangle_right = self.Region(selected.x2, selected.y1, frame.x2, selected.y2)
+            rectangle_below = self.Region(frame.x1, selected.y2, frame.x2, frame.y2)
+            yield from self._split_rectangles(rectangle_above, self._clip_rectangles(rectangle_above, rects))
+            yield from self._split_rectangles(rectangle_left, self._clip_rectangles(rectangle_left, rects))
+            yield from self._split_rectangles(rectangle_right, self._clip_rectangles(rectangle_right, rects))
+            yield from self._split_rectangles(rectangle_below, self._clip_rectangles(rectangle_below, rects))
 
 
 if __name__ == '__main__':
